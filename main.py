b3f9b155a2ea210b22fadaaf12e51a967a918983
EmiLeeUh <87795510+EmiLeeUh@users.noreply.github.com>
2025-08-16 15:08:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,4 +23,5 @@ def npc():
     return jsonify({"reply": reply})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
